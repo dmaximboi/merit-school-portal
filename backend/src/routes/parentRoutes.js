b0b12a77870a5b1114f7f982f6185014b5e771ee
maxim010 <140669557+Uhhghhujbh@users.noreply.github.com>
2025-12-13 00:30:02 -0800

    Update parentRoutes.js

--- backend/src/routes/parentRoutes.js
+++ backend/src/routes/parentRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const parentController = require('../controllers/parentController');
 
+// Routes
 router.post('/login', parentController.parentLogin);
+router.post('/update-password', parentController.updatePassword); 
 
 module.exports = router;
